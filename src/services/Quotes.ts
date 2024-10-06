40db980a6163d367ceb1c544a797fcb5c876c864
Masfik H <contact@masfik.net>
2024-10-06 23:32:36 +0100

    Implement new quotes functions

--- src/services/Quotes.ts
+++ src/services/Quotes.ts
@@ -1,10 +1,22 @@
 import { randomInteger } from "./utils";
 import LocalStorageService from "./LocalStorageService";
 
-export type Quote = { quote: string; caption: string };
+export type Quote = {
+    id: number;
+    quote: string;
+    caption?: string;
+    who: string;
+};
 
 export default class Quotes extends LocalStorageService {
     private readonly quotes = this.db.getCollection<Quote>("quotes");
+
+    addQuote = (quote: Quote) => this.quotes.insert(quote);
+
+    deleteQuote(id: number) {
+        if (!this.quotes.findOne({ id })) throw "ID not found";
+        this.quotes.chain().find({ id }).remove();
+    }
 
     getRandomQuote() {
         const all = this.getAll();
