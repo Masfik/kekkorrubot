3b451f2ab7e90a4d5a64a54aa0576d0ea7ba66d4
Masfik H <contact@masfik.net>
2024-10-07 19:54:56 +0100

    Add commands list

--- src/commands/manage.ts
+++ src/commands/manage.ts
@@ -14,7 +14,15 @@ export default async function manage(ctx: CommandContext) {
             `Qui potrai modificare e visualizzare le impostazioni del bot.\n` +
             `<b>Dev mode = ${process.env.DEVMODE}</b>\n\n` +
             `${quizQuantity}\n` +
-            `${quotesQuantity}\n`,
+            `${quotesQuantity}\n\n\n` +
+            `🛡️ <b>Comandi Admin</b>\n` +
+            `\/adquiz - aggiungi un nuovo quiz\n` +
+            `\/quizlist - lista quiz\n` +
+            `\/deletequiz id - elimina quiz specificato\n` +
+            `\/adquote - aggiungi una nuova citazione\n` +
+            `\/quotes - lista delle citazioni\n` +
+            `\/deletequote id - elimina la citazione specificata\n` +
+            `\/allowgroup - esegui questo comando nel gruppo che vuoi autorizzare ad utilizzare i quiz e shipping`,
         { parse_mode: "HTML" },
     );
 }
--- src/services/Unsplash.ts
+++ src/services/Unsplash.ts
@@ -9,9 +9,6 @@ export default class Unsplash {
     });
 
     async fetchRandomPhotoURL() {
-        if (process.env.DEVMODE)
-            return "https://cdn.ebaumsworld.com/mediaFiles/picture/2452130/85386505.jpg";
-
         const { data } = await this.axios.get("/photos/random");
         return JSON.parse(data).urls.regular;
     }
